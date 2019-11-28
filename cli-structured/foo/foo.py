73d4ab6e80a75eaa392045860f1169560886e2b0
odelucca <deluccafonseca@gmail.com>
2019-11-28 16:01:18 -0200

    fix

---
+++ cli-structured/foo/foo.py
@@ -0,0 +1,11 @@
+import cli
+import core
+
+def say_hi():
+    print('hi')
+
+
+if __name__ == '__main__':
+    say_hi()
+
+from foo import cli, core
---
+++ cli-structured/foo/tests/test_foo.py
@@ -0,0 +1,8 @@
+from foo import foo
+
+
+def test_foo():
+    foo.say_hi()
+    pri
+
+    assert False

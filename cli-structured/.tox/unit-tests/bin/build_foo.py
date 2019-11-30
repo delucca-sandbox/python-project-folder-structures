76a8b61baeb3da98d4e507653f9fbc7a5984208b
odelucca <deluccafonseca@gmail.com>
2019-11-30 15:14:12 -0300

    fix

--- cli-structured/.tox/unit-tests/bin/build_foo.py
+++
@@ -1,7 +0,0 @@
-#!/home/odelucca/Code/sandbox/python-project-folder-structures/cli-structured/.tox/unit-tests/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'foo==1.0.0','build_foo.py'
-__requires__ = 'foo==1.0.0'
-__import__('pkg_resources').require('foo==1.0.0')
-__file__ = '/home/odelucca/Code/sandbox/python-project-folder-structures/cli-structured/scripts/build_foo.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
